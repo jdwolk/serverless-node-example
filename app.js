35b1e437f0ff1c1c8d780d886cac03f4ab195321
JD Wolk <jd@philosophie.is>
2018-10-26 13:58:25 -0700

    Adds query param to function call

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  const name = req.query.name || 'world'
+  res.json({ hello: name })
 })
 
 module.exports = app
